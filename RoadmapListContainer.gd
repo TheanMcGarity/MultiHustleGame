2d7844d693cd9933f1bb5279bf42e9869b3b0f30
MadMincraftr <thean.mcgarity@gmail.com>
2025-11-18 16:18:53 -0600

    0.8.0

--- RoadmapListContainer.gd
+++ RoadmapListContainer.gd
@@ -1,8 +1,11 @@
-extends VBoxContainer
+extends HBoxContainer
 
 export var slide_time = 0.0
 export var off_x = -230
 export var on_x = 0
+
+export var off_object_positions = {}
+export var on_object_positions = {}
 
 var state:bool = false
 
@@ -15,8 +18,16 @@ func toggle(val):
 	var tween := create_tween()
 	match state:
 		true:
-			tween.tween_property(self, "rect_position", Vector2(off_x, y_pos), slide_time).set_trans(Tween.EASE_IN_OUT)
+			tween.parallel().tween_property(self, "rect_position", Vector2(off_x, y_pos), slide_time).set_trans(Tween.EASE_IN_OUT)
+			
+			for pos in off_object_positions.keys():
+				tween.parallel().tween_property(get_node(pos), "rect_position", Vector2(off_object_positions[pos], get_node(pos).rect_position.y), slide_time).set_trans(Tween.EASE_IN_OUT)
+			
 			state = false
 		false:
-			tween.tween_property(self, "rect_position", Vector2(on_x, y_pos), slide_time).set_trans(Tween.EASE_IN_OUT)
+			tween.parallel().tween_property(self, "rect_position", Vector2(on_x, y_pos), slide_time).set_trans(Tween.EASE_IN_OUT)
+			
+			for pos in on_object_positions.keys():
+				tween.parallel().tween_property(get_node(pos), "rect_position", Vector2(on_object_positions[pos], get_node(pos).rect_position.y), slide_time).set_trans(Tween.EASE_IN_OUT)
+			
 			state = true
