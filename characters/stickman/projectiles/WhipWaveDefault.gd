5907044ffbd30471f573139c6e426deb4861cc69
Ian Sly <iansly@protonmail.com>
2026-05-17 17:48:43 -0700

    1.9.80-unstable

--- characters/stickman/projectiles/WhipWaveDefault.gd
+++ characters/stickman/projectiles/WhipWaveDefault.gd
@@ -1,4 +1,8 @@
 extends DefaultFireball
 
-func _frame_6():
+func _enter():
+	if host.whip_parriable:
+		host.has_projectile_parry_window = true
+
+func _frame_4():
 	host.has_projectile_parry_window = true
---
+++ characters/stickman/states/WhipWaveParriable.gd
@@ -0,0 +1,6 @@
+extends CharacterState
+
+func process_projectile(obj):
+	.process_projectile(obj)
+	obj.whip_parriable = true
+	pass
