2fd2add9951b54506777d56a5f2cf83a2c8842d0
Ian Sly <iansly@protonmail.com>
2026-05-18 15:24:49 -0700

    1.9.82-unstable

--- Global.gd
+++ Global.gd
@@ -2,7 +2,7 @@ extends Node
 
 signal nag_window()
 
-var VERSION = "1.9.81-steam-unstable"
+var VERSION = "1.9.82-steam-unstable"
 const RESOLUTION = Vector2(640, 360)
 
 const STYLE_SAVE_FEATURE_ENABLED = true
--- characters/wizard/EnergyBomb.gd
+++ characters/wizard/EnergyBomb.gd
@@ -7,3 +7,22 @@ func process_projectile(projectile):
 	host.add_spark_bomb(projectile.obj_name)
 	interruptible_on_opponent_turn = true
 	pass
+
+
+export var trigger_orb_attack = true
+export var trigger_orb_type = ""
+export var trigger_orb_delay = 20
+var triggered_orb_attack = false
+
+func _enter_shared():
+	._enter_shared()
+	triggered_orb_attack = false
+	pass
+
+func _tick_shared():
+	if !triggered_orb_attack and current_tick == 0:
+		if trigger_orb_attack:
+			if host.orb_projectile:
+				host.objs_map[host.orb_projectile].trigger_attack(trigger_orb_type, trigger_orb_delay)
+				triggered_orb_attack = true
+	return ._tick_shared()
