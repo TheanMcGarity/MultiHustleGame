fef3288ee8fc61828bef32de9270b96ac1170d32
Ian Sly <iansly@protonmail.com>
2026-06-03 14:52:24 -0700

    1.9.93-unstable

--- characters/states/Fall.gd
+++ characters/states/Fall.gd
@@ -3,5 +3,5 @@ extends CharacterState
 func _tick():
 	host.apply_grav()
 	host.apply_forces()
-	if current_tick > 3 and host.is_grounded():
+	if (current_tick > 3 or _previous_state_name() == "Fall") and host.is_grounded():
 		return "Landing"
